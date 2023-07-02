bc16bafda5144697faefc0ca96480c62be7f9514
SiHero999 <137537393+SiHero999@users.noreply.github.com>
2023-07-02 10:07:42 +0300

    Update SpringBoot4ApplicationTests.java

--- spring-boot4/src/test/java/com/me/SpringBoot4ApplicationTests.java
+++ spring-boot4/src/test/java/com/me/SpringBoot4ApplicationTests.java
@@ -2,7 +2,7 @@ package com.me;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//this is new comment ahhahaha
 @SpringBootTest
 class SpringBoot4ApplicationTests {
 
